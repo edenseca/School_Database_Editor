55cb988b5ed55e1a6591aae84f9fa77d806db1d8
ShawnB <brutuss@wit.edu>
2023-07-11 13:49:47 -0400

    test

--- Student.py
+++ Student.py
@@ -1,7 +1,9 @@
 from User import *
-
+import sqlite3
 
 class Student(User):
+    con = sqlite3.connect('assignment3.db')
+    cur = con.cursor()
     def __init__(self, id, first_name, last_name, email, grad_year, major, crn):
         super().__init__(id, first_name, last_name, email)
         self.grad_year = grad_year
@@ -25,9 +27,49 @@ class Student(User):
         if j == 0:
             print('Could not find: ', in_crn)
 
-    def show_schedule(self):
-        i = 0
-        while i < len(self.crn):
-            print(self.crn[i])
-            i = i + 1
+    def course_search(self):
+        def course_search():  # Everyone
+            while True:
+                print("What attribute will you filter for?\n"
+                      "1) CRNs\n"
+                      "2) Title\n"
+                      "3) Department\n"
+                      "4) Semester\n"
+                      "5) Exit")
+                course_op = int(input(''))
+                if course_op == 1:
+                    search_crn = int(input("Enter CRN: "))
+                    cur.execute("SELECT * FROM course WHERE crn = '{}'".format(search_crn))
+                    for row in cur.fetchall():
+                        print(row)
+                elif course_op == 2:
+                    search_title = input("Enter Title: ")
+                    cur.execute("SELECT * FROM course WHERE title = '{}'".format(search_title))
+                    for row in cur.fetchall():
+                        print(row)
+                elif course_op == 3:
+                    search_depart = input("Enter department : ")
+                    cur.execute("SELECT * FROM course WHERE department  = '{}'".format(search_depart))
+                    for row in cur.fetchall():
+                        print(row)
+                    cur.execute("SELECT * FROM instructor WHERE dept = '{}'".format(search_depart))
+                    for row in cur.fetchall():
+                        print(row)
+                elif course_op == 4:
+                    search_semester = input("Enter semester : ")
+                    cur.execute("SELECT * FROM course WHERE semester = '{}'".format(search_semester))
+                    for row in cur.fetchall():
+                        print(row)
+                elif course_op == 5:
+                    break
+                else:
+                    print("Invalid Input!")
+
+    def display_courses(self):  # Everyone
+        cur.execute("SELECT * FROM course")
+        z = cur.fetchall()
+
+        print("COURSES:")
+        for row in z:
+            print(row)
 
--- Teacher.py
+++ Teacher.py
@@ -9,20 +9,108 @@ class Teacher(User):
         self.department = department
 
     def show_schedule(self):
-        print('Showing Teachers Schedule')
-        self.id = self.id
+        def course_search():  # Everyone
+            while True:
+                print("What attribute will you filter for?\n"
+                      "1) CRNs\n"
+                      "2) Title\n"
+                      "3) Department\n"
+                      "4) Semester\n"
+                      "5) Exit")
+                course_op = int(input(''))
+                if course_op == 1:
+                    search_crn = int(input("Enter CRN: "))
+                    cur.execute("SELECT * FROM course WHERE crn = '{}'".format(search_crn))
+                    for row in cur.fetchall():
+                        print(row)
+                elif course_op == 2:
+                    search_title = input("Enter Title: ")
+                    cur.execute("SELECT * FROM course WHERE title = '{}'".format(search_title))
+                    for row in cur.fetchall():
+                        print(row)
+                elif course_op == 3:
+                    search_depart = input("Enter department : ")
+                    cur.execute("SELECT * FROM course WHERE department  = '{}'".format(search_depart))
+                    for row in cur.fetchall():
+                        print(row)
+                    cur.execute("SELECT * FROM instructor WHERE dept = '{}'".format(search_depart))
+                    for row in cur.fetchall():
+                        print(row)
+                elif course_op == 4:
+                    search_semester = input("Enter semester : ")
+                    cur.execute("SELECT * FROM course WHERE semester = '{}'".format(search_semester))
+                    for row in cur.fetchall():
+                        print(row)
+                elif course_op == 5:
+                    break
+                else:
+                    print("Invalid Input!")
 
 
     def print_class_list(self):
-        self.id = self.id
-        con = sqlite3.connect('assignment3.db')
-        cur = con.cursor()
-        cur.execute("SELECT * FROM student")
-        t = cur.fetchall()
-        print("STUDENTS:")
-        for row in t:
-            print(row)
+        def display_student():  # Teachers
+            cur.execute("SELECT * FROM student")
+            t = cur.fetchall()
+
+            print("STUDENTS:")
+            for row in t:
+                print(row)
 
     def search_crn(self):
-        print('Search for CRN')
-        self.id = self.id
+        def display_courses(self):  # Everyone
+            cur.execute("SELECT * FROM course")
+            z = cur.fetchall()
+
+            print("COURSES:")
+            for row in z:
+                print(row)
+
+
+
+
+    def student_search():  # Admin/Teacher
+        while True:
+            print("What attribute will you filter for?\n"
+                  "1) First Name\n"
+                  "2) Last Name\n"
+                  "3) WID\n"
+                  "4) Major\n"
+                  "5) Graduation Year\n"
+                  "6) Email\n"
+                  "7) BACK")
+            filter_op = int(input(''))
+            if filter_op == 1:
+                search_name = input("Enter the first name: ")
+                cur.execute("SELECT * FROM student WHERE name = '{}'".format(search_name))
+                for row in cur.fetchall():
+                    print(row)
+            elif filter_op == 2:
+                search_name = input("Enter the last name: ")
+                cur.execute("SELECT * FROM student WHERE surname = '{}'".format(search_name))
+                for row in cur.fetchall():
+                    print(row)
+            elif filter_op == 3:
+                search_wid = int(input("Enter WITid: "))
+                cur.execute("SELECT * FROM student WHERE id = '{}'".format(search_wid))
+                for row in cur.fetchall():
+                    print(row)
+            elif filter_op == 4:
+                search_major = input("Enter Major: ")
+                cur.execute("SELECT * FROM student WHERE major = '{}'".format(search_major))
+                for row in cur.fetchall():
+                    print(row)
+            elif filter_op == 5:
+                search_grad_year = int(input("Enter expected grad year: "))
+                cur.execute("SELECT * FROM student WHERE gradyear = '{}'".format(search_grad_year))
+                for row in cur.fetchall():
+                    print(row)
+
+            elif filter_op == 6:
+                search_email = input("Enter student email to search: ")
+                cur.execute("SELECT * FROM student WHERE email = '{}'".format(search_email))
+                for row in cur.fetchall():
+                    print(row)
+            elif filter_op == 7:
+                break
+            else:
+                print("Invalid Input!")
